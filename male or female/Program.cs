2ddf52008011e64891ea03c8ce7ade719f8ed43e
Abdul asik raja <asikraja2001@gmail.com>
2024-12-18 22:39:50 +0530

    Add project files.

---
+++ male or female/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace male_or_female
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your gender (Male/Female): ");
+            string gender = Console.ReadLine();
+
+            if (gender.Equals("Male", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("User is Male");
+            }
+            else if (gender.Equals("Female", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("User is Female");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
